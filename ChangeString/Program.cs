7bc8f8f6194e66fc1992838ba2d8796e805f446d
Gabriela Branco <lindosogb@gmail.com>
2024-03-07 18:17:46 +0000

    Replaces the character given by the user in a string for 'X'

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -6,7 +6,26 @@ namespace ChangeString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, give me a string!");
+            string stringInput = Console.ReadLine();
+
+            Console.WriteLine("Now give me a character!");
+            char character = Console.ReadLine()[0];
+
+            string finalString = "";
+            for (int i = 0; i < stringInput.Length; i++)
+            {
+                if (stringInput[i] == character)
+                {
+                    finalString += "X";
+                }
+                else
+                {
+                    finalString += stringInput[i];
+                }
+            }
+
+            Console.WriteLine(finalString);
         }
     }
 }
