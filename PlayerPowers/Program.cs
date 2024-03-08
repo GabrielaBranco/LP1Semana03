36d3bf33e3df9231e8093d61ad234522e8e3547e
Gabriela Branco <lindosogb@gmail.com>
2024-03-08 15:04:38 +0000

    test commit

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -6,14 +6,28 @@ namespace PlayerPowers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Give me a number:");
-            string input = Console.ReadLine();
+            Console.WriteLine("Give me the number of players:");
+            string players = Console.ReadLine();
 
-            int n = int.Parse(input);
+            int n = int.Parse(players);
 
             string[] myPowers = {Powers.Fly.ToString(), Powers.SuperStrength.ToString(), Powers.XRayVision.ToString()};
 
-            
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Choose your powers player " + (i+1) + "->");
+                for(int j = 0; j < myPowers.Length; j++)
+                {
+                    Console.WriteLine(j + ". " + myPowers[j]);
+                }
+
+                string plyrPowers = Console.ReadLine();
+
+                char[] powers = plyrPowers.ToCharArray();
+
+                Console.WriteLine(powers[0]);
+
+            }
         }
     }
 }
