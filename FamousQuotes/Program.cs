68b9dd30c1545ad0980e9d3df6acd8c4d71134a6
Gabriela Branco <lindosogb@gmail.com>
2024-03-08 00:28:37 +0000

    Exercise to call methods inside main class

---
+++ FamousQuotes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FamousQuotes
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("You talking to me?");//Taxi driver?
+            MyPrecious();
+            Console.WriteLine("Dodge this");//Matrix
+            Hasta();
+        }
+
+        private static void Hasta()
+        {
+            TheMoney();
+            Console.WriteLine("Hasta la vista, baby.");//The Terminator
+            MyPrecious();
+        }
+
+        private static void TheMoney()
+        {
+            Console.WriteLine("Show me the money!");//no idea!
+        }
+
+        private static void MyPrecious()
+        {
+            TheMoney();
+            Console.WriteLine("My precious.");//LOTR
+        }
+    }
+}
